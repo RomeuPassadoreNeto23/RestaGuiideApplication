3a53b024a019af7276420444b7b0d6f3f1b19dc6
Romeu <passadore.neto@outlook.com>
2022-04-07 11:17:08 -0300

    Melhoria no formulario Restaurante

--- src/main/java/br/senai/sp/restaguide/repository/RestauranteRepository.java
+++ src/main/java/br/senai/sp/restaguide/repository/RestauranteRepository.java
@@ -1,9 +1,12 @@
 package br.senai.sp.restaguide.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.senai.sp.restaguide.model.Restaurante;
 
 public interface RestauranteRepository  extends PagingAndSortingRepository<Restaurante, Long>{
+ 
 
 }
